21e47ddfcaa2d07693a0b2421acaecc898ea2183
= <=>
2023-06-16 22:22:14 +1000

    Added proper website meta-description

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
         <br />
         Bill Martinson for the Cosmodex, which this site started upon.
         <br />
-        Version: 1.1.0
+        Version: 1.1.0a
       </span>
     </footer>
   );
