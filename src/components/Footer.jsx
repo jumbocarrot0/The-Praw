b4f66ca9678939f5a69a5f0690964113b056797e
= <=>
2023-08-04 07:30:58 +1000

    Fixed some typos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
         <br />
         Bill Martinson for the Cosmodex, which this site started upon.
         <br />
-        Version: 1.2.2
+        Version: 1.2.2a
       </span>
     </footer>
   );
