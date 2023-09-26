3850bbaaf3dc2c67576055856b6b0517fcac3724
= <=>
2023-09-26 15:43:33 +1000

    Clarified win limitation rules

--- src/pages/Variants/WinLimitationPage.js
+++ src/pages/Variants/WinLimitationPage.js
@@ -14,12 +14,13 @@ export default function WinLimitationPage() {
       </ul>
       <h3>Gameplay</h3>
       <ul>
-        <li className="text-light">When the offense has four foreign colonies (or three in a four-planet game), they may only invite a number of allies up to the number the token on the colony tracker is next to.</li>
-        <li className="text-light">When the offense has four foreign colonies (or three in a four-planet game), the defense may only invite a number of allies up to one more than the number the token on the colony tracker is next to.</li>
+        <li className="text-light">When the offense is one foreign colony away from winning, they may only invite a number of allies up to the number the token on the colony tracker is next to.</li>
+        <li className="text-light">When the offense is one foreign colony away from winning, the defense may only invite a number of allies up to one more than the number the token on the colony tracker is next to.</li>
           <ul>
             <li className="text-light">So for example, if the offense has four foreign colonies and the token is on the "2", then the Offense may invite up to two allies and the defense may invite up to three.</li>
+            <li className="text-light">If any effects make the number of foreign colonies needed to win unknown (e.g. Schizoid), just ignore these effects for the win limiation.</li>
           </ul>
-        <li className="text-light">After the offense loses an encounter, if they had four foreign colonies (or three in a four-planet game), advance the token on the colony tracker to the next space.</li>
+        <li className="text-light">After the offense loses an encounter, if they are one foreign colony away from winning, advance the token on the colony tracker to the next space.</li>
         <li className="text-light">If the token is on the five space and should advance, instead discard it. There are now no limits on allies.</li>
       </ul>
     </div>
