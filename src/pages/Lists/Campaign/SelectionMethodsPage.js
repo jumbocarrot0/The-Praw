574f3180fe77268cbd1742f38a050f558169f1e3
= <=>
2023-09-26 21:47:39 +1000

    Selection methods are linked on page

--- src/pages/Lists/Campaign/SelectionMethodsPage.js
+++ src/pages/Lists/Campaign/SelectionMethodsPage.js
@@ -1,36 +1,70 @@
 import Ages from '../../../dataFiles/ages.json';
 import { Link } from 'react-router-dom';
 import { useEffect, useRef } from 'react';
+// import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+// import { Toast, ToastBody, ToastHeader } from 'reactstrap';
+
+function LinkIcon() {
+  return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+  </svg>
+}
+
+// function CopyIcon() {
+//   return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
+//     <path fillRule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V2Zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H6ZM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1H2Z" />
+//   </svg>
+// }
 
 export default function SelectionMethodsPage() {
-  const { hash } = useLocation();
+  const { 
+    // pathname, 
+    hash } = useLocation();
 
   const ref = useRef(null)
 
   useEffect(() => {
-    if (hash) {
+    // if (hash) {
       document.documentElement.scrollTo({
         top: ref.current?.getBoundingClientRect().top - 100,
         left: 0,
         behavior: "smooth", // Optional if you want to skip the scrolling animation
       });
       // ref.current.scrollIntoView()
-      console.log()
-    }
-  }, [hash])
+    // }
+  }, [])
 
   function SelectionMethod(props) {
     const method = props.method
-    const i = props.i
+    // const [toastOpen, setToastOpen] = useState(false)
+    // const toggle = () => setToastOpen(!toastOpen);
+
     return <div
-      className={ hash.slice(1) === method.original.name ? 'p-2 mb-2 border border-2 border-indigo' : 'p-2 mb-2'}
-      style={hash.slice(1) === method.original.name ? { boxShadow: "0px 0px 10px #f600ff" } : null}
-      ref={hash.slice(1) === method.original.name ? ref : null}
-      key={i}
+      className={hash.slice(1) === method.original.name.replaceAll(' ', '_') ? 'p-2 mb-2 border border-2 border-indigo' : 'p-2 mb-2'}
+      style={hash.slice(1) === method.original.name.replaceAll(' ', '_') ? { boxShadow: "0px 0px 10px #f600ff" } : null}
+      ref={hash.slice(1) === method.original.name.replaceAll(' ', '_') ? ref : null}
       id={method.original.name}
     >
-      <h3>{method.original.name}</h3>
+      <Link
+        // onClick={() => {
+        //   navigator.clipboard.writeText(`${window.location.host}${pathname}#${method.original.name.replaceAll(' ', '_')}`);
+        //   setToastOpen(true)
+        // }
+        // }
+        to={`#${method.original.name.replaceAll(' ', '_')}`}
+      >
+        <h3>{method.original.name} <LinkIcon /></h3>
+      </Link>
+      {/* <Toast isOpen={toastOpen}>
+        <ToastHeader toggle={toggle}>
+          Toast title
+        </ToastHeader>
+        <ToastBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ToastBody>
+      </Toast> */}
       <p>{method.revised?.body || method.original.body}</p>
     </div>
   }
@@ -47,12 +81,12 @@ export default function SelectionMethodsPage() {
       <h2>Official Selection Methods</h2>
       <p>These have been revised slightly to fix some oddities.</p>
       {
-        Object.values(Ages.selectionMethods).filter((method) => method.original.expansion !== "Fan Made").map((method, i) => <SelectionMethod method={method} i={i} />)
+        Object.values(Ages.selectionMethods).filter((method) => method.original.expansion !== "Fan Made").map((method, i) => <SelectionMethod method={method} key={i} />)
       }
       <h2>Fan-made Selection Methods</h2>
       <p>These have been created by fans without a corresponding age card, and also includes the <Link to="https://futurepastimes.com/blog/2023/4/24/new-campaign-alien-selection-methods">"semi-official"</Link> methods posted on the Future Pastimes blog..</p>
       {
-        Object.values(Ages.selectionMethods).filter((method) => method.original.expansion === "Fan Made").sort(function (a, b) {return a.original.name > b.original.name}).map((method, i) => <SelectionMethod method={method} i={i} />)
+        Object.values(Ages.selectionMethods).filter((method) => method.original.expansion === "Fan Made").sort(function (a, b) { return a.original.name > b.original.name }).map((method, i) => <SelectionMethod method={method} key={i} />)
       }
     </div>
   );
