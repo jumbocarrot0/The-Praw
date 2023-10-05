b57abf4ad4465f1d3f6aac9ccc941d1e73a98e6b
jumbocarrot0 <jumbocarrot0@gmail.com>
2023-10-05 12:36:09 +1000

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
         <br />
         Bill Martinson for the Cosmodex, which this site started upon.
         <br />
-        Version: 1.4.4c
+        Version: 1.4.4d
       </span>
     </footer>
   );
