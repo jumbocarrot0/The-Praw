893d2bbf3bf6f63eb64f969c9d4a96c7b26372e1
= <=>
2023-12-10 12:00:44 +1000

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
         <br />
         Bill Martinson for the Cosmodex, which this site started upon.
         <br />
-        Version: 1.4.7a
+        Version: 1.4.8
       </>
     </footer>
   );
